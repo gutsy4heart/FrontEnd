2f0deb2ea1664c5f52be7d9a3b882be707757aa3
Murad <murad.nurmammadli@gmail.com>
2024-11-18 21:39:34 +0400

    pacman 2.0

--- Javascript/Новая папка/index.js
+++ Javascript/Новая папка/index.js
@@ -1,32 +1,26 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width=800;
-canvas.height=400;
-
+canvas.width = 800;
+canvas.height = 400;
 ctx.lineWidth = 10;
 
-ctx.strokeStyle = 'white'
-ctx.fillStyle = 'white'
-
-// Radian - 
-
 const mouthWidth = Math.PI / 4;
-const mouthWidthHalf = mouthWidth / 2;
 
-const sector = (color, fillAngle = 0, turnAngle = 0) =>{
-    const fillAngleHalf = fillAngle /2;
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.arc(200, 200, 50, fillAngleHalf,Math.PI + fillAngleHalf);
-    ctx.closePath();
-    ctx.fill();
-
-    ctx.fillStyle = 'yellow';
-    ctx.beginPath();
-    ctx.arc(200, 200, 50, -fillAngleHalf, Math.PI- fillAngleHalf, true );
-    ctx.closePath();
-    ctx.fill();
-
+const sector = (color, x, y, startAngle = 0, endAngle = 0, cc = false) => {
+	ctx.fillStyle = color;
+	ctx.beginPath();
+	ctx.moveTo(x, y);
+	ctx.arc(x, y,50,startAngle,endAngle, cc);
+	ctx.lineTo(x, y);
+	ctx.closePath();
+	ctx.fill();
 }
-sector('red', Math.PI / 2);
+
+const pacman = (color, x, y, mouthWidth) => {
+	sector(color, x, y,-mouthWidth / 2, Math.PI, true);
+	sector(color, x, y,mouthWidth / 2, Math.PI);
+}
+
+pacman('red', 50, 50, mouthWidth);
+pacman('yellow', 250, 250, mouthWidth);
